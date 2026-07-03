3817892c54dcacd3dedc41a02e52668f6baa67a3
Anita Redno Sari <anitarednosari001@gmail.com>
2026-07-03 11:36:15 +0700

    Add navbar.php for website navigation

---
+++ DailyDelish/assets/includes/navbar.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Daily Delish</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+
+</head>
+
+<body>
+
+<header>
+
+    <nav class="navbar">
+
+        <a href="index.php" class="logo">
+            Daily <span>Delish</span>
+        </a>
+
+        <ul class="nav-menu">
+
+            <li><a href="index.php">Home</a></li>
+
+            <li><a href="recipes.php">Recipes</a></li>
+
+            <li><a href="categories.php">Categories</a></li>
+
+            <li><a href="about.php">About</a></li>
+
+            <li><a href="contact.php">Contact</a></li>
+
+        </ul>
+
+        <div class="nav-right">
+
+            <a href="#" class="search-btn">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </a>
+
+            <a href="login.php" class="login-btn">
+                Login
+            </a>
+
+            <a href="register.php" class="register-btn">
+                Register
+            </a>
+
+        </div>
+
+        <div class="menu-toggle">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+
+    </nav>
+
+</header>
