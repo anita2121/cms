971bb7c664834161112fbc551b1b3a34b2ae993b
Anita Redno Sari <anitarednosari001@gmail.com>
2026-07-03 11:33:27 +0700

    Create navbar.php

---
+++ DailyDelish/assets/includes/navbar.php
@@ -0,0 +1,52 @@
+<footer>
+
+    <div class="container">
+
+        <h2>Daily Delish</h2>
+
+        <p>
+            Discover Delicious Recipes Every Day
+        </p>
+
+        <br>
+
+        <div class="social">
+
+            <a href="#">
+                <i class="fab fa-facebook"></i>
+            </a>
+
+            <a href="#">
+                <i class="fab fa-instagram"></i>
+            </a>
+
+            <a href="#">
+                <i class="fab fa-youtube"></i>
+            </a>
+
+            <a href="#">
+                <i class="fab fa-x-twitter"></i>
+            </a>
+
+        </div>
+
+        <br>
+
+        <p>
+
+            © 2026 Daily Delish
+
+            <br>
+
+            All Rights Reserved.
+
+        </p>
+
+    </div>
+
+</footer>
+
+<script src="assets/js/script.js"></script>
+
+</body>
+</html>
